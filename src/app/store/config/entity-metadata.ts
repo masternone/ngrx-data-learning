5d3b7ce7bb532f78331a02d346d957caa1cb4e85
Lisa Lemons <lemonsl@dematic.com>
2020-03-27 12:44:37 -0400

    The custom filter

--- src/app/store/config/entity-metadata.ts
+++ src/app/store/config/entity-metadata.ts
@@ -2,6 +2,52 @@ import {EntityDataModuleConfig, EntityMetadataMap, PropsFilterFnFactory} from '@
 import * as species from '../species';
 import * as person from '../person';
 
+export type CustomEntityFilterFn<T> = (
+  entities: T[],
+  pattern?: any,
+  value?: any
+) => T[];
+/**
+ * Creates an {EntityFilterFn} that matches RegExp or RegExp string pattern
+ * anywhere in any of the given props of an entity.
+ * If pattern is a string, spaces are significant and ignores case.
+ */
+export function CustomPropsFilterFnFactory<T = any>(
+  props: (keyof T)[] = []
+): CustomEntityFilterFn<T> {
+  if (props.length === 0) {
+    // No properties -> nothing could match -> return unfiltered
+    return (entities: T[]) => entities;
+  }
+
+  return (entities: T[], pattern: string | RegExp, value?: any) => {
+    if (!entities) {
+      return [];
+    }
+
+    if (value === null || value === undefined || value.length === '') {
+      const regExp =
+        typeof pattern === 'string' ? new RegExp(pattern, 'i') : pattern;
+      if (regExp) {
+        const predicate = (e: any) => props.some(prop => regExp.test(e[prop]));
+        return entities.filter(predicate);
+      }
+      return entities;
+    } else {
+      const predicate = (e: any) =>
+        props.some(prop => {
+          // Find the matching property name (will be the pattern)
+          if (prop !== pattern) {
+            return;
+          }
+          return e[prop] === value;
+        });
+      return entities.filter(predicate);
+    }
+  };
+}
+
+
 // This creates the filter for ... filtering
 function nameFilter(entities: { name: string }[], search: string) {
   return entities.filter(e => -1 < e.name.indexOf(search));
